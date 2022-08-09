76438b38de13763eb4fd1b36667b9310f15a810d
jjooccaa <95224827+jjooccaa@users.noreply.github.com>
2022-08-09 14:21:02 +0200

    Initial Commit

--- Assets/Scripts/ObjectPooler.cs
+++ Assets/Scripts/ObjectPooler.cs
@@ -7,15 +7,18 @@ public class ObjectPooler : MonoBehaviour
     public static ObjectPooler SharedInstance;
 
     [Header("Roads")]
-    [SerializeField] GameObject[] roadsToPool;
+    [SerializeField] int numberOfRoadsToPool;
+    [SerializeField] GameObject[] roadPrefabs;
     [SerializeField] List<GameObject> pooledRoads;
 
     [Header("Cities")]
-    [SerializeField] GameObject[] citiesToPool;
+    [SerializeField] int numberOfCitiesToPool;
+    [SerializeField] GameObject[] cityPrefabs;
     [SerializeField] List<GameObject> pooledCities;
 
     [Header("Obstacles")]
-    [SerializeField] GameObject[] obstaclesToPool;
+    [SerializeField] int numberOfObstaclesToPool;
+    [SerializeField] GameObject[] obstaclesPrefabs;
     [SerializeField] List<GameObject> pooledObstacles;
 
     private void Awake()
@@ -23,76 +26,77 @@ public class ObjectPooler : MonoBehaviour
         SharedInstance = this;
 
         pooledRoads = new List<GameObject>();
-        PoolObjects(roadsToPool, pooledRoads);
+        PoolObjects(roadPrefabs, pooledRoads, numberOfRoadsToPool);
 
         pooledCities = new List<GameObject>();
-        PoolObjects(citiesToPool, pooledCities);
+        PoolObjects(cityPrefabs, pooledCities, numberOfCitiesToPool);
 
         pooledObstacles = new List<GameObject>();
-        PoolObjects(obstaclesToPool, pooledObstacles);
+        PoolObjects(obstaclesPrefabs, pooledObstacles, numberOfObstaclesToPool);
     }
 
-    void PoolObjects(GameObject[] objectsToPool, List<GameObject> pooledObjects)
+    void PoolObjects(GameObject[] objectPrefabs, List<GameObject> pooledObjects, int numberOfObjectsToPool)
     {
         //Loop through list of pooled objects, deactivating them and adding them to the list
-        for (int i = 0; i < objectsToPool.Length; i++)
+        int indexOfPrefabs = 0;
+        for (int i = 0; i < numberOfObjectsToPool; i++, indexOfPrefabs++)
         {
-            GameObject obj = Instantiate(objectsToPool[i],objectsToPool[i].transform.position,objectsToPool[i].transform.rotation);
+            if(indexOfPrefabs + 1 > objectPrefabs.Length) // if we reach end of Objects Prefabs, reset index
+            {
+                indexOfPrefabs = 0;
+            }
+            GameObject obj = Instantiate(objectPrefabs[indexOfPrefabs], objectPrefabs[indexOfPrefabs].transform.position, objectPrefabs[indexOfPrefabs].transform.rotation);
             obj.SetActive(false);
             pooledObjects.Add(obj);
-            obj.transform.SetParent(this.transform); // set as children of Spawn Manager
+            obj.transform.SetParent(this.transform); // set as children of Spawn Manager 
         }
     }
 
     public GameObject GetPooledRoad()
     {
-        int randomIndex;
 
-        while (true)
-        {
-            randomIndex = Random.Range(0, pooledRoads.Count);
-            if (!pooledRoads[randomIndex].activeInHierarchy)
-            {
-
-                break;
-            }
-        }
-
-        return pooledRoads[randomIndex];
+        return GetRandomPoolOjbect(pooledRoads);
     }
 
     public GameObject GetPooledCity()
     {
-        int randomIndex;
 
-        while (true)
-        {
-            randomIndex = Random.Range(0, pooledCities.Count);
-            if (!pooledCities[randomIndex].activeInHierarchy)
-            {
-
-                break;
-            }
-        }
-
-        return pooledCities[randomIndex];
+       return GetRandomPoolOjbect(pooledCities);
     }
 
     public GameObject GetPooledObstacles()
     {
-        int randomIndex;
 
-        while (true)
+        return GetRandomPoolOjbect(pooledObstacles);
+    }
+
+    GameObject GetRandomPoolOjbect(List<GameObject> pooledObjects)
+    {
+        while (CheckForInactiveObjects(pooledObjects))
         {
-            randomIndex = Random.Range(0, pooledObstacles.Count);
-            if (!pooledObstacles[randomIndex].activeInHierarchy)
+            int randomIndex = Random.Range(0, pooledObjects.Count);
+
+            if (!pooledObjects[randomIndex].activeInHierarchy) 
+            { 
+
+                return pooledObjects[randomIndex];
+            }
+        }
+        
+        return null;
+    }
+
+    bool CheckForInactiveObjects(List<GameObject> objectsList) 
+    {
+        for(int i = 0; i < objectsList.Count; i++)
+        {
+            if(!objectsList[i].activeInHierarchy)
             {
 
-                break;
+                return true;
             }
-
         }
 
-        return pooledObstacles[randomIndex];
+        return false;
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -43,12 +43,13 @@ public class SpawnManager : MonoBehaviour
         if (pooledRoad != null)
         {
             previousRoad = pooledRoad;
+            pooledRoad = null;
         }
+
         // Get new pooled road obj, make it active and position it based on last Z position
         pooledRoad = ObjectPooler.SharedInstance.GetPooledRoad();
         pooledRoad.SetActive(true);
-        pooledRoad.transform.position = (previousRoad != null) ? NewZPosition(previousRoad.transform.position, zOffset)
-            : NewZPosition(pooledRoad.transform.position, zOffset);
+        MoveToNewZPosition(pooledRoad, previousRoad, zOffset);
     }
 
     void PoolCity(float zOffset)
@@ -56,12 +57,13 @@ public class SpawnManager : MonoBehaviour
         if (pooledCity != null)
         {
             previousCity = pooledCity;
+            pooledCity = null;
         }
+
         // Get new pooled city obj, make it active and position it based on last Z position
         pooledCity = ObjectPooler.SharedInstance.GetPooledCity();
         pooledCity.SetActive(true);
-        pooledCity.transform.position = (previousCity != null) ? NewZPosition(previousCity.transform.position, zOffset)
-            : NewZPosition(pooledCity.transform.position, zOffset);
+        MoveToNewZPosition(pooledCity, previousCity, zOffset);
     }
 
     void PoolObstacles(float zOffset)
@@ -71,24 +73,27 @@ public class SpawnManager : MonoBehaviour
             previousObstacles = pooledObstacles;
             pooledObstacles = null;
         }
+
         // Get new pooled obstacles obj, make it active and position it based on last Z position
         pooledObstacles = ObjectPooler.SharedInstance.GetPooledObstacles();
         pooledObstacles.SetActive(true);
-        pooledObstacles.transform.position = (previousObstacles != null) ? NewZPosition(previousObstacles.transform.position, zOffset)
-            : NewZPosition(pooledObstacles.transform.position, zOffset);
-    }    
-    
+        MoveToNewZPosition(pooledObstacles, previousObstacles, zOffset);
+    }
+
+    void MoveToNewZPosition(GameObject pooledObj, GameObject previousPooledObj, float zOffset)
+    {
+        Vector3 newZPos = new(0, 0, zOffset);
+
+        // If we have previous object, add z offset to that object's position and if we don't, add it to current object position
+        pooledObj.transform.position = (previousPooledObj != null) ? previousPooledObj.transform.position + newZPos
+            : pooledObj.transform.position + newZPos;
+    }
+
     void DeactivateObject(GameObject obj)
     {
         if(obj != null)
         {
             obj.SetActive(false);
         }
-    }
-
-    Vector3 NewZPosition(Vector3 oldPosition, float zOffset)
-    {
-
-        return oldPosition + new Vector3(0, 0, zOffset);
     }
 }
